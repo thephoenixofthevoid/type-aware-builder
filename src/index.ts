e2361a431596e4cc41d4385c7df0aa8240626277
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2021-06-04 10:50:35 +0300

    Add implementation

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+type SetterKey<K> = `set${Capitalize<string & K>}`;
+
+type FilterOptional<T> = {
+    [K in keyof T]: undefined extends T[K] ? never : T[K]
+}
+
+type SetterFunctionType<O, A, R, K> = K extends A ? (
+        (v: O[keyof O & K]) => Builder<O, 
+            keyof O & Exclude<A, K>, 
+            keyof O & Exclude<R, K>
+        >
+    ) : never
+
+export type Builder<O, A = keyof O, R = keyof FilterOptional<O>> = {
+        [K in keyof O as SetterKey<K>]: 
+            SetterFunctionType<O, A, R, K>
+    } & {
+        done: {} extends Pick<O, keyof O & R> ? () => O : never; //: never;
+    }
+
+export function createBuilder<T extends object>(obj: T): Builder<T> {
+  return new Proxy(obj as Builder<T>, { get })
+
+    function get(target, property, receiver) {
+
+        if (isFinish(property)) return getFinish<T>(target, property, receiver);
+        
+        if (isSetter(property)) return getSetter<T>(target, property, receiver) 
+        
+        return undefined
+    }
+}
+
+function isFinish(property: string) {
+    return "done" === property
+}
+
+function isSetter(property: string) {
+    return typeof property === "string" && /^set[A-Z]/.test(property)
+}
+
+function getFinish<T extends object>(target, property, receiver) {
+    return () => target as T
+}
+
+
+function getSetter<T extends object>(target, property, receiver) {
+
+    const name = property.slice(3, 4).toLowerCase() + property.slice(4)
+    return function(value: any) {
+
+        Reflect.defineProperty(target as T, name, {
+            value: value,
+            writable: true,
+            configurable: true,
+            enumerable: true
+        })
+
+        return receiver
+    }
+}
